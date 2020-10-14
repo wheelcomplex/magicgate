a82f797a0327f8c2e6ac03ed187e6c0667c2c848
David Mortals <david@mortals.city>
2020-10-14 20:51:26 +1300

    move utils from magicgate to standalone utils package

--- utils/utils.go
+++ utils/utils.go
@@ -1,12 +1,50 @@
 // utils for magicgate
 
-package magicgate
+package utils
 
 import (
 	"crypto/rand"
 	"encoding/hex"
+	"fmt"
+	"os"
+	"os/signal"
 	"strings"
+	"syscall"
 )
+
+// NoSIGHUP will ignore HUP signal
+func NoSIGHUP() {
+	c := make(chan os.Signal, 8)
+	signal.Notify(c, syscall.SIGHUP)
+	go func() {
+		for s := range c {
+			fmt.Printf("NoSIGHUP, ignored: %v\n", s)
+		}
+	}()
+}
+
+// NoSIGPIPE will ignore PIPE signal
+func NoSIGPIPE() {
+	c := make(chan os.Signal, 8)
+	signal.Notify(c, syscall.SIGPIPE)
+	go func() {
+		for s := range c {
+			fmt.Printf("NoSIGPIPE, ignored: %v\n", s)
+		}
+	}()
+}
+
+// NopError represent a error which is not a real error
+type NopError struct{}
+
+func (m *NopError) Error() string {
+	return "NOP"
+}
+
+// IsNopError return true is err is a NopError
+func IsNopError(err error) bool {
+	return err.Error() == "NOP"
+}
 
 // LoopReplaceAll repeatedly replace until nothing changed
 func LoopReplaceAll(s, old, new string) string {
